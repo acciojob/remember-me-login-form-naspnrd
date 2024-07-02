80d3db0e8bed8f654d8b42fecc530b32fa184768
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-07-02 23:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const rememberMeCheckbox = document.querySelector("#checkbox");
+const existingButton = document.querySelector("#existing");
+existingButton.style.display = "none";
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const username = usernameInput.value;
+	const password = passwordInput.value;
+	const rememberMe = rememberMeCheckbox.checked; // true or false
+	if(rememberMe) {
+		localStorage.setItem("username", username)
+		localStorage.setItem("password", password)
+	} else {
+		localStorage.removeItem("username", username)
+		localStorage.removeItem("password", password)
+	}
+	alert(`Logged in as ${username}.`);
+	showButton();
+	form.reset();
+})
+
+function showButton() {
+	const username = localStorage.getItem("username")
+	const password = localStorage.getItem("password")
+	if(username && password) {
+		existingButton.style.display = "block";
+	} else {
+		existingButton.style.display = "none";
+	}
+}
+function loginAsSaved() {
+	const username = localStorage.getItem("username")
+	const password = localStorage.getItem("password")
+	if(username && password) {
+		alert(`Logged in as ${username}.`);
+	}
+}
+existingButton.addEventListener('click', loginAsSaved)
